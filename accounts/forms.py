f420ef4239ddfdc8043af1eb00f18a3f93c47fdf
Alawadi86 <malawadi380@gmail.com>
2022-11-05 17:31:46 -0400

    fixing the typo

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,12 +2,14 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
 
-class CustomUserCreationFrom(UserCreationForm):
+
+class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ("birthday",)
-        
+
+
 class CustomUserChangeForm(UserChangeForm):
-        class Meta:
-            model = CustomUser
-            fields = UserChangeForm.Meta.fields
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
--- accounts/models.py
+++ accounts/models.py
@@ -1,24 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import datetime
 
 
 class CustomUser(AbstractUser):
-    birthday = models.DateTimeField(null=True, blank=True)
-
-    def calculate_age(self):
-        """ Calculate age based on birthday"""
+    birthday = models.DateField(max_length=8)
+    age = models.IntegerField()
+    def __str__(self):
         today = date.today()
 
-        try: 
-            birthday = self.dob.replace(year=today.year)
-        # raised when birth date is February 29 and the current year is not a leap year
-        except ValueError:
-            birthday = self.dob.replace(year=today.year, day=born.day-1)
+        age = today.year - dob.year
+        if today.month < dob.month or today.month == dob.month and today.day < dob.day:
+            age -= 1
+        return self.age 
 
-        if birthday > today:
-            return today.year - born.year - 1
-        else:
-            return today.year - born.year
+
+
+ 
 
 
 
