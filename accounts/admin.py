a6503955a14f9e41ceedf749ad2cabbc4375610c
Alawadi86 <malawadi380@gmail.com>
2022-11-05 16:46:33 -0400

    added the templates and registration

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,25 +2,22 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationFrom
-    form = CustomUserChangeForm
+    add_form = UserCreationForm
+    form = UserChangeForm
     model = CustomUser
-    list_display =[
+    list_display = [
         "email",
         "username",
         "birthday",
         "is_staff",
     ]
-    fieldsets = UserAdmin.fieldsets + ( (None, {"fields": ("birthday",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ( (None, {"fields": ("birthday",)}),)
-       
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("birthday",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("birthday",)}),)
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
-                
-  
-    
--- django_project/settings.py
+++ django_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR ],
+        "DIRS": [BASE_DIR/ "templates" ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -125,3 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# log in and log out settings
+LOGIN_REDRIECT_URL ="home"
+LOGOUT_REDIRECT_URL ="home"
